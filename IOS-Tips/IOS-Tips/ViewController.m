3f3b5c751180062a055142f3c3a1fb38692a6b50
Phu Nguyen <pisun2@gmail.com>
2013-07-05 15:52:46 +0700

    Update ViewController.m

--- IOS-Tips/IOS-Tips/ViewController.m
+++ IOS-Tips/IOS-Tips/ViewController.m
@@ -20,6 +20,38 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)saveImage:(UIImage *)image withFilename:(NSString *)filename{
+    
+	NSString *path;
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"WeatherHTTPClientImages/"];
+	
+    
+    BOOL isDir;
+    
+    if(![[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir]){
+        if(!isDir){
+            NSError *error;
+            [[NSFileManager defaultManager] createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:&error];
+            
+            NSLog(@"%@",error);
+        }
+    }
+    
+    path = [path stringByAppendingPathComponent:filename];
+    NSData *imageData = UIImagePNGRepresentation(image);
+    NSLog(@"Written: %d",[imageData writeToFile:path atomically:YES]);
+}
+
+-(UIImage *)imageWithFilename:(NSString *)filename{
+    NSString *path;
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"WeatherHTTPClientImages"];
+	path = [path stringByAppendingPathComponent:filename];
+    
+    return [UIImage imageWithContentsOfFile:path];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
